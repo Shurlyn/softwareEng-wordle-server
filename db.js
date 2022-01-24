e60b4a7725da694d0e6c9cc42cb2eec9ef489cb1
Ram Narayanan <ram_narayanan@horacemann.org>
2022-01-24 13:37:03 -0500

    Add db code

--- db.js
+++ db.js
@@ -5,3 +5,12 @@ const db = new sqlite.Database(dbFile, (error) => {
   if (error) return console.error(error.message);
   console.log(`Connected to database ${dbFile}`);
 });
+const word = ["c", "h", "e", "c", "k"];
+
+const guess = (request, response) => {
+    const userGuess = request.body.guess;
+    guessArray = userGuess.split("");
+    for(let x = 0; x < 5; x++){
+        
+    }
+};
