79b35f309ebcff0db6f16db0db9d99f5372c3df8
Shurlyn <sherlynsfashion@gmail.com>
2022-01-25 09:38:46 -0500

    add check logic

--- db.js
+++ db.js
@@ -10,7 +10,20 @@ const word = ["c", "h", "e", "c", "k"];
 const guess = (request, response) => {
     const userGuess = request.body.guess;
     guessArray = userGuess.split("");
+
+    var result = []
+
     for(let x = 0; x < 5; x++){
-        
+        // includes()
+
+        // that word and in that index
+        if (word.includes(userGuess[x], x)) {
+          result.append("Green")
+        } else if (word.includes(userGuess[x])) { // if it's just in the word
+          result.append("Yellow")
+        } else { // if it's not
+          result.append("Gray")
+        }
+
     }
 };
